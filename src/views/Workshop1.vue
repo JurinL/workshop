61fb8ac965348d4de06d127b94fe2609ebbf751c
Jurin Laiyakosit <jurin37@hotmail.co.th>
2024-11-26 14:09:14 +0700

    workshop1 test

--- src/views/Workshop1.vue
+++ src/views/Workshop1.vue
@@ -1,58 +1,99 @@
 <template>
   <div>
-    <h1>My Profile</h1>
-    <div class="container">
-      <v-card class="d-flex" elevation="12" height="45vh">
-        <v-img
-          src="../assets/profile.jpg"
-          width="34vb"
-          contain
-        ></v-img>
+    <h1>Profile</h1>
+    <v-container>
+      <v-row>
+        <!-- Profile Cards -->
+        <v-col v-for="(person, index) in people" :key="index" cols="3">
+          <v-card @click="showProfile(person)" class="profile-card">
+            <v-img :src="person.image" height="40vh" width="25vb"></v-img>
+            <v-card-title>{{ person.name }}</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
 
-        <v-card-text>
-          <v-card-title class="Title">{{ name }}</v-card-title>
-          <v-card-subtitle>{{ university }}</v-card-subtitle>
-          <v-card-text> งานอดิเรก : {{ hobby }}</v-card-text>
-          <v-card-text>
-              <v-list-item>
-                <v-icon left>mdi-email</v-icon>{{ email }}
-                <v-icon left>mdi-phone</v-icon>{{ phone }}
-              </v-list-item>
-              <v-card-actions style="display: flex; justify-content: center">
-                <v-btn color="success" width="15vw"><v-icon size="30">mdi-thumb-up</v-icon>Like</v-btn>
-                <v-btn color="red" width="15vw"><v-icon size="30">mdi-thumb-down</v-icon>Dislike</v-btn>
-              </v-card-actions>
-          </v-card-text>
-        </v-card-text>
-      </v-card>
-    </div>
+      <!-- Profile Dialog -->
+      <v-dialog v-model="dialog" max-width="1500">
+        <div class="container">
+          <v-card class="d-flex" height="50vh" width="75%" v-if="selectedPerson">
+            <v-img :src="selectedPerson.image" width="20vw" height="50vh"></v-img>
+
+            <v-card-text>
+              <v-card-title class="Title">{{ selectedPerson.name }}</v-card-title>
+              <v-divider></v-divider>
+              <v-card-subtitle>{{ selectedPerson.university }}</v-card-subtitle>
+
+              <v-list>
+                <v-list-item>
+                  <v-icon color="green">mdi-thumb-up</v-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Likes</v-list-item-title>
+                    <v-list-item-subtitle>{{ selectedPerson.likes }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-icon color="red">mdi-thumb-down</v-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Dislikes</v-list-item-title>
+                    <v-list-item-subtitle>{{ selectedPerson.dislikes }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-icon color="yellow">mdi-star</v-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Hobbies</v-list-item-title>
+                    <v-list-item-subtitle>{{ selectedPerson.hobby }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </div>
+      </v-dialog>
+    </v-container>
   </div>
 </template>
-  
-<script>
+
+  <script>
+import JurinImg from '../assets/profile.jpg'
 export default {
-  name: "Profile",
+  name: 'Test',
   data() {
     return {
-      name: "Jurin Laiyakosit",
-      university: "มหาวิทยาลัยสงขลานครินทร์ สาขาวิศวกรรมคอมพิวเตอร์",
-      email: "jurin37@hotmail.co.th",
-      phone: "+66950244763",
-      hobby: "อ่านหนังสือ ฟังเพลง เล่นเกม",
-      like: "แมว",
-      dislike: "แมลงสาบ",
-      show: false,
-    };
+      dialog: false,
+      selectedPerson: null,
+      people: [
+        {
+          name: 'Jurin Laiyakosit',
+          image: JurinImg,
+          university: 'Prince of Songkla University, Computer Engineering',
+          email: 'jurin37@hotmail.co.th',
+          phone: '+66950244763',
+          hobby: 'Reading, Gaming, Music',
+          likes: 'Cats, Books, Weekends',
+          dislikes: 'Bugs, Traffic, Mondays'
+        },
+      ]
+    }
   },
-};
+  methods: {
+    showProfile(person) {
+      this.selectedPerson = person
+      this.dialog = true
+    }
+  }
+}
 </script>
-  
+
 <style scoped>
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 60vh;
+.v-card {
+  transition: transform 0.2s;
+}
+.v-card:hover {
+  transform: scale(1.02);
 }
 h1 {
   text-align: center;
@@ -73,5 +114,17 @@ h1 {
   font-size: 2.5vw;
   margin-right: 1vw;
   margin-left: 1vw;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+}
+.profile-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
 }
 </style>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,87 +1,159 @@
-Sure! Here's the modified code to place the information next to the image:
-
-```html
-<template>
+  <template>
   <div>
-    <h1>My Profile</h1>
-    <div class="container">
-      <v-card class="d-flex" elevation="12" height="55vh">
-        <v-row>
-          <v-col cols="4">
-            <v-img src="../assets/profile.jpg" width="100%" height="100%" contain @click="show = !show"></v-img>
-          </v-col>
-          <v-col cols="8">
-            <v-expand-transition>
-              <v-card-text v-if="show">
-                <v-card-title class="Title">{{ name }}</v-card-title>
-                <v-card-subtitle>{{ university }}</v-card-subtitle>
-                <v-card-text> งานอดิเรก : {{ hobby }}</v-card-text>
-                <v-card-text>
-                  <v-row>
-                    <v-list-item>
-                      <v-icon left>mdi-email</v-icon>{{ email }}
-                      <v-icon left>mdi-phone</v-icon>{{ phone }}
-                    </v-list-item>
-                    <v-card-actions style="display: flex; justify-content: center">
-                      <v-btn color="success" width="15vw">
-                        <v-icon size="30">mdi-thumb-up</v-icon>Like
-                      </v-btn>
-                      <v-btn color="red" width="15vw">
-                        <v-icon size="30">mdi-thumb-down</v-icon>Dislike
-                      </v-btn>
-                    </v-card-actions>
-                  </v-row>
-                </v-card-text>
-              </v-card-text>
-            </v-expand-transition>
-          </v-col>
-        </v-row>
-      </v-card>
-    </div>
+    <h1>Team Profiles</h1>
+    <v-container>
+      <v-row>
+        <!-- Profile Cards -->
+        <v-col
+          v-for="(person, index) in people"
+          :key="index"
+          cols="12"
+          sm="6"
+          md="3"
+        >
+          <v-card @click="showProfile(person)" hover class="profile-card">
+            <v-img
+              :src="person.image"
+              height="40vh"
+              width="25vb"
+              contain
+            ></v-img>
+            <v-card-title class="text-center">{{ person.name }}</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <!-- Profile Dialog -->
+      <v-dialog v-model="dialog" max-width="1500">
+        <div class="container">
+          <v-card
+            class="d-flex"
+            height="50vh"
+            width="75%"
+            v-if="selectedPerson"
+          >
+            <v-img
+              :src="selectedPerson.image"
+              width="20vw"
+              height="50vh"
+              contain
+            ></v-img>
+
+            <v-card-text>
+              <v-card-title class="Title">{{
+                selectedPerson.name
+              }}</v-card-title>
+              <v-divider></v-divider>
+              <v-card-subtitle>{{ selectedPerson.university }}</v-card-subtitle>
+
+              <v-list>
+                <v-list-item>
+                  <v-icon>mdi-thumb-up</v-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Likes</v-list-item-title>
+                    <v-list-item-subtitle>{{
+                      selectedPerson.likes
+                    }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-icon>mdi-thumb-down</v-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Dislikes</v-list-item-title>
+                    <v-list-item-subtitle>{{
+                      selectedPerson.dislikes
+                    }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-icon>mdi-star</v-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Hobbies</v-list-item-title>
+                    <v-list-item-subtitle>{{
+                      selectedPerson.hobby
+                    }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </div>
+      </v-dialog>
+    </v-container>
   </div>
 </template>
 
-<script>
+  <script>
+import profileImage from '../assets/profile.jpg'
+
 export default {
+  name: 'Test',
   data() {
     return {
-      show: false,
-      name: 'Your Name',
-      university: 'Your University',
-      hobby: 'Your Hobby',
-      email: 'your.email@example.com',
-      phone: '123-456-7890'
-    };
+      dialog: false,
+      selectedPerson: null,
+      people: [
+        {
+          name: 'Jurin Laiyakosit',
+          image: profileImage,
+          university: 'Prince of Songkla University, Computer Engineering',
+          email: 'jurin37@hotmail.co.th',
+          phone: '+66950244763',
+          hobby: 'Reading, Gaming, Music',
+          likes: 'Cats, Books, Weekends',
+          dislikes: 'Bugs, Traffic, Mondays'
+        },
+    ]
+    }
+  },
+  methods: {
+    showProfile(person) {
+      this.selectedPerson = person
+      this.dialog = true
+    }
   }
-};
+}
 </script>
 
 <style scoped>
+.v-card {
+  transition: transform 0.2s;
+}
+.v-card:hover {
+  transform: scale(1.02);
+}
+h1 {
+  text-align: center;
+  font-size: 4vw;
+  font-weight: bold;
+  margin-top: 30wh;
+  color: white;
+  background-color: black;
+  font-family: monospace;
+}
+.Title {
+  font-size: 4vw;
+  margin-bottom: 4%;
+  color: black;
+  font-family: fantasy;
+}
+.v-icon {
+  font-size: 2.5vw;
+  margin-right: 1vw;
+  margin-left: 1vw;
+}
 .container {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 60vh;
 }
-
-.Title {
-  font-size: 24px;
-  font-weight: bold;
-}
-
-.v-btn {
-  margin: 10px;
-}
-
-.v-card-actions {
+.profile-card {
   display: flex;
-  justify-content: center;
-}
-
-.v-icon {
-  margin-right: 5px;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
 }
 </style>
-```
-
-This code uses Vuetify's grid system to place the image in a column on the left and the text in a column on the right. Let me know if you need any further adjustments!
