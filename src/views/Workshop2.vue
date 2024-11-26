7d98a7eeadc3c6df0dfda19b5c13b5b5a14019c6
Jurin Laiyakosit <jurin37@hotmail.co.th>
2024-11-26 11:04:01 +0700

    workshop2 update

---
+++ src/views/Workshop2.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <v-text-field 
+      name="score"
+      label="กรอกคะแนน"
+      id="score"
+      v-model="score"
+    ></v-text-field>
+    <div><v-btn color="success" @click="GradeCalculator()">ตัดเกรด</v-btn></div>
+      <h1>{{ grade }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      score: 0,
+      grade: "",
+    };
+  },
+  methods: {
+    GradeCalculator() {
+      if (this.score > 100 || this.score < 0) {
+        alert("คะแนนห้ามต่ำกว่า 0 หรือมากกว่า 100");
+      } else if (this.score >= 80) {
+        this.grade = "A";
+      } else if (this.score >= 70) {
+        this.grade = "B";
+      } else if (this.score >= 60) {
+        this.grade = "C";
+      } else if (this.score >= 50) {
+        this.grade = "D";
+      } else if (this.score >= 0) {
+        this.grade = "F";
+      } else {
+        alert("รับค่าเฉพาะตัวเลข");
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+}
+</style>
